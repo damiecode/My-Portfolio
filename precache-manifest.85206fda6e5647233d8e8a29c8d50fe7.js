c224d935b39d381328e775bb86cf625fc73d44d0
damiecode <codenlyn@gmail.com>
2020-08-12 15:00:41 +0100

    Updates

---
+++ precache-manifest.85206fda6e5647233d8e8a29c8d50fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1628cba5661e4bf5280768314e02618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06a862e2f6c47b8b7d0c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3c87166ab4ebb7c4d5c",
+    "url": "/static/js/2.8d424c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d424c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a862e2f6c47b8b7d0c",
+    "url": "/static/js/main.e201740b.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
