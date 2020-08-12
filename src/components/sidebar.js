e122eeb30c554d6c8b7a9a789913853f732ef4a1
damiecode <codenlyn@gmail.com>
2020-08-12 18:24:07 +0100

    Change background image

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
 const Sidebar = () => (
@@ -7,11 +8,11 @@ const Sidebar = () => (
       <aside id="colorlib-aside" className="border js-fullheight">
         <div className="text-center">
           <div className="author-img" style={{ backgroundImage: 'url(images/about.jpg)' }} />
-          <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
+          <h1 id="colorlib-logo"><a href="index.html">Damilola Ale</a></h1>
           <span className="email">
             <i className="icon-mail" />
             {' '}
-            dhruv.barochia34788@gmail.com
+            codenlyn@gmail.com
           </span>
         </div>
         <nav id="colorlib-main-menu" role="navigation" className="navbar">
@@ -28,36 +29,13 @@ const Sidebar = () => (
         <nav id="colorlib-main-menu">
           <ul>
             <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-            <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-            <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-            <li><a href="https://www.linkedin.com/in/dbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-            <li><a href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-github" /></a></li>
-            <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2" /></a></li>
+            <li><a href="https://twitter.com/iamlildamski" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
+            <li><a href="https://www.instagram.com/dam_eva/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
+            <li><a href="https://www.linkedin.com/in/damiecode/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+            <li><a href="https://github.com/damiecodes" target="_blank" rel="noopener noreferrer"><i className="icon-github" /></a></li>
+            <li><a href="https://medium.com/@codenlyn" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2" /></a></li>
           </ul>
         </nav>
-        <div className="colorlib-footer">
-          <p>
-            <small>
-              Made with
-              {' '}
-              <i className="icon-heart" aria-hidden="true" />
-              {' '}
-              and
-              <i className="icon-beer" aria-hidden="true" />
-              <br />
-              Thanks
-              {' '}
-              <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>
-              {' '}
-              for inspiration
-            </small>
-          </p>
-          <p>
-            <small>
-              Something coming soon !!
-            </small>
-          </p>
-        </div>
       </aside>
     </div>
   </div>
