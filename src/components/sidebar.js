4f89a0c67afcd6eb93db6fac0d2f22432364a96a
damiecode <codenlyn@gmail.com>
2020-08-14 18:33:52 +0100

    Add contact form

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -23,7 +23,7 @@ const Sidebar = () => (
               <li><a href="#about" data-nav-section="about">About</a></li>
               <li><a href="#projects" data-nav-section="projects">Projects</a></li>
               <li><a href="#education" data-nav-section="education">Education</a></li>
-              <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+              <li><a href="#contact" data-nav-section="contact">Contact</a></li>
             </ul>
           </div>
         </nav>
